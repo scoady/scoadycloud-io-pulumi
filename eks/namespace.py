9fb0c0093aa7d33732d7d9ff822d8532f0e7437b
Sean Coady <scoady@apple.com>
2021-11-22 10:49:09 -0800

    More testing
    Test
    
    Test
    
    Test
    
    Test
    
    Test
    
    test
    
    test
    
    test
    
    test
    
    test
    
    test
    
    test
    
    test
    
    test
    
    remove junk

--- eks/namespace.py
+++ eks/namespace.py
@@ -1,10 +1,16 @@
 from pulumi_kubernetes.core.v1 import Namespace
+import pulumi_kubernetes as k8s
 import pulumi
+from pulumi import Config,Output
+import config as config_util
+import utils
 
 def create_namespace(namespace_name) -> Namespace:
     """
     Create a namespace with recommended labels in EKS cluster
     """
+    config = Config()
+    eks_cluster_name = config.require("cluster_name")
     labels = {
         "name": namespace_name,
     }

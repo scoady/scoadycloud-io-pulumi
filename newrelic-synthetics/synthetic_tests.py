ab5fbe596c2a4b20e62447982ad8e7f53fd33168
Sean Coady <scoady@apple.com>
2021-12-07 00:24:10 -0800

    last batch

--- newrelic-synthetics/synthetic_tests.py
+++ newrelic-synthetics/synthetic_tests.py
@@ -1,32 +1,22 @@
 import pulumi
 import pulumi_newrelic as newrelic
-
+from pulumi import Config,export
 
 def create():
-    us_east_hc = newrelic.synthetics.Monitor("tinyurl-us-east-1-frontdoor-monitor",
-        name="tinyurl-us-east-1-synthetic-frontdor-monitor",
-        frequency=5,
-        locations=[
-            "AWS_US_EAST_1",
-            "AWS_US_EAST_2",
-        ],
-        status="ENABLED",
-        type="SIMPLE",
-        uri="https://tinyurl-us-west-1.web.scoady.io/healthz",
-        validation_string="Ok",
-        verify_ssl=True)
-    # Optional for type "SIMPLE" and "BROWSER"
+    config=Config()
+    synthetic_checks=config.require_object('checks')
+    for synthetic_check in synthetic_checks:
+        check = newrelic.synthetics.Monitor(f"{synthetic_check['name']}",
+            name=f"{synthetic_check['name']}-monitor",
+            frequency=5,
+            locations=[
+                "AWS_US_EAST_1",
+                "AWS_US_EAST_2",
+            ],
+            status="ENABLED",
+            type="SIMPLE",
+            uri=f"{synthetic_check['endpoint']}",
+            validation_string=f"{synthetic_check['validation_string']}",
+            verify_ssl=True)
+        export(f"{synthetic_check['name']}",check)
 
-    us_west_hc = newrelic.synthetics.Monitor("tinyurl-us-west-1-synthetic-frontdoor-monitor",
-        name="tinyurl-us-west-1-synthetic-frontdoor-monitor",
-        frequency=5,
-        locations=[
-            "AWS_US_EAST_1",
-            "AWS_US_EAST_2",
-        ],
-        status="ENABLED",
-        type="SIMPLE",
-        uri="https://tinyurl-us-west-1.web.scoady.io/-/healthy",
-        validation_string="Ok",
-        verify_ssl=True)
-    # Optional for type "SIMPLE" and "BROWSER"
